89a6b1521c17414b981b5e6bf9a94b0b5d8ee3ce
guanhe <307784732@qq.com>
2016-09-08 14:29:17 +0800

    add samba.sh

---
+++ distro/common/samba.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+apt-get install samba
+#smbclient
+#expect
+
+USER="guanhe"
+PASSWD="123456"
+NEW_DIR="share"
+CONF_FILE="/etc/samba/smb.conf"
+
+deluser $USER
+
+/usr/bin/expect << EOD
+set timeout 10
+spawn adduser $USER
+expect "password:"
+send "$PASSWD\n"
+expect "password:"
+send "$PASSWD\n"
+expect "Full Name"
+send "\n"
+expect "Room Number"
+send "\n"
+expect "Work Phone"
+send "\n"
+expect "Home Phone"
+send "\n"
+expect "Other"
+send "\n"
+expect "information correct"
+send "Y\n"
+expect eof
+EOD
+
+
+/usr/bin/expect <<EOF
+spawn smbpasswd -a $USER
+expect "New SMB password"
+send "$PASSWD\n"
+expect "Retype new SMB password"
+send "$PASSWD\n"
+expect eof
+EOF
+
+if [ ! -d /opt/$NEW_DIR ];then
+mkdir /opt/$NEW_DIR
+fi
+
+cat<<EOF>>$CONF_FILE
+[share]
+comment = /root
+writeable = yes
+security = share
+path = /opt/$NEW_DIR
+browseable = yes
+public = yes
+EOF
+
+/etc/init.d/smbd restart
+if [ $? -ne 0 ];then
+    exit 0
+fi
+
+/usr/bin/expect <<EOF
+spawn smbclient //127.0.0.1/$NEW_DIR
+expect "smb: \>"
+send "exit\n"
+expect eof
+EOF
+deluser $USER 
